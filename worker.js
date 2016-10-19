4564259ba6f0a1bd19c2ed97e64e83331d158166
Mike Elsmore <mike.elsmore@uk.ibm.com>
2016-10-19 13:15:21 +0100

    Add clustering

--- worker.js
+++ worker.js
@@ -2,6 +2,8 @@
 
 require('dotenv').load({ silent: true });
 
+var cluster = require('cluster');
+
 var amqp = require('amqplib/callback_api');
 
 var Nexmo = require('nexmo');
@@ -12,24 +14,34 @@ var nexmo = new Nexmo({
   debug: true,
 });
 
-amqp.connect(process.env.RABBIT, function(err, conn) {
-  if (err) console.error(err);
-  conn.createChannel(function(err, ch) {
-    var q = 'notifications';
+var numCPUs = 4;
 
-    ch.assertQueue(q, {durable: false});
+if (cluster.isMaster) {
+    for (var i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+} else {
 
-    console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
-    ch.consume(q, function(msg) {
-      console.log(" [x] Received %s", msg.content.toString());
-      var sms = JSON.parse(msg.content.toString());
-      nexmo.message.sendSms(sms.from, sms.to, sms.msg, {}, function(err, resp) {
-        if (err) {
-          console.error(err, resp);
-        } else {
-          console.log(" [x] SMS sent to %s", sms.to);
-        }
-      });
-    }, {noAck: true});
+  amqp.connect(process.env.RABBIT, function(err, conn) {
+    if (err) console.error(err);
+    conn.createChannel(function(err, ch) {
+      var q = 'notifications';
+
+      ch.assertQueue(q, {durable: false});
+
+      console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
+      ch.consume(q, function(msg) {
+        console.log(" [x] Received %s", msg.content.toString());
+        var sms = JSON.parse(msg.content.toString());
+        nexmo.message.sendSms(sms.from, sms.to, sms.msg, {}, function(err, resp) {
+          if (err) {
+            console.error(err, resp);
+          } else {
+            console.log(" [x] SMS sent to %s", sms.to);
+          }
+        });
+      }, {noAck: true});
+    });
   });
-});
+
+}
